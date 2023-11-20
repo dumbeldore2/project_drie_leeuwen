d39a19bdc7d54220ea58f69978b67b1753f05b62
dumbeldore2 <yago.engels@gmail.com>
2023-11-20 21:54:42 +0100

    COMMIT 4

--- app/src/main/java/com/example/project_drie_leeuwen/MainActivity2.java
+++ app/src/main/java/com/example/project_drie_leeuwen/MainActivity2.java
@@ -3,12 +3,25 @@ package com.example.project_drie_leeuwen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    //init de layouts
+    Button button, button2;
+    ListView listView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //conect layouts
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+
     }
 }
